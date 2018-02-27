299cad34c33bb5c572734788ed3757e796964759
Igor Martins <dev.igor.martins@gmail.com>
2018-02-26 22:51:12 -0300

    Criando operação de instanciação

--- src/main/java/com/sistema/ws/WsApplication.java
+++ src/main/java/com/sistema/ws/WsApplication.java
@@ -1,12 +1,32 @@
 package com.sistema.ws;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class WsApplication {
+import com.sistema.ws.domain.Categoria;
+import com.sistema.ws.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class WsApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(WsApplication.class, args);
 	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Carnes");
+		Categoria cat2 = new Categoria(null, "Leguminosas");
+		
+		categoriaRepository.save(Arrays.asList(cat1, cat2));
+	}
+	
 }
